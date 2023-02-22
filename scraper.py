0fa80e279a5324636cf435ce49067856d0baf978
jay <ronokamau@gmail.com>
2023-02-22 12:40:20 +0300

    trying out web scraping

---
+++ scraper.py
@@ -0,0 +1,8 @@
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+
+html = urlopen('http://www.pythonscraping.com/exercises/exercise1.html')
+bsObj = BeautifulSoup(html.read())
+print(bsObj.h1)
